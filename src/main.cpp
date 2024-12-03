9fb98f5fb814d3f436788007ef39704187dfdd5e
AKCA Samet <Smt.68.58@gmail.com>
2024-12-03 13:51:33 +0100

    Activite5_ESP32

---
+++ src/main.cpp
@@ -0,0 +1,46 @@
+#include <Arduino.h>
+
+const int Full_trame = 11;
+int trame[Full_trame];
+int compteur = 0;
+int tab[5];
+int carte[5] = { 0xC, 0x0, 0x42, 0x16, 0x94 };
+
+
+void setup() {
+  Serial.begin(9600);
+  Serial2.begin(9600, SERIAL_8N1, 2, -1); // RX sur GPIO 2, TX désactivé (-1)
+  Serial.println("Systeme verouiller, presenter badge");
+}
+
+void loop() {}
+
+void serialEventRun() {
+  while (Serial2.available()) {
+    trame[compteur] = Serial2.read();
+    compteur++;
+    if (compteur == Full_trame) {
+      for (int i = 0; i < 5; i++) {
+        tab[i] = trame[i + 3];
+      }
+
+      bool whitelist = true;
+      for (int i = 0; i < 5; i++) {
+        if (tab[i] != carte[i]) {
+          whitelist = false;
+          break;
+        }
+      }
+
+      if (whitelist) {
+        for (int i = 0; i < 5; i++) {
+          Serial.println("Carte correcte, Systeme deverouiller ");
+        } 
+      }
+      else {
+          Serial.println("Carte incorrecte, Systeme verouiller ");
+        }
+      compteur = 0;
+    }
+  }
+}
