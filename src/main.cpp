05abfe912f13ff674ac7d5b71e35922ece498bf4
AKCA Samet <Smt.68.58@gmail.com>
2024-12-10 15:21:41 +0100

    Whitelist

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,8 @@
 const int Full_trame = 11;
 int trame[Full_trame];
 int compteur = 0;
+int tab[5];
+int carte[5] = {0xC, 0x0, 0x42, 0x16, 0x94};
 
 void setup() {
   Serial.begin(9600);
@@ -14,7 +16,6 @@ void serialEvent() {
   while (Serial.available()) {
     trame[compteur] = Serial.read();
     compteur++;
-
     if (compteur == Full_trame) {
       for (int i = 0; i < Full_trame; i++) {
         Serial.print(trame[i], HEX);
