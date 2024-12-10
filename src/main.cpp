f213ea34b8bc35d9844cf2ab81f3779d218f3936
AKCA Samet <Smt.68.58@gmail.com>
2024-12-10 15:26:49 +0100

    Robot ON/OFF

--- src/main.cpp
+++ src/main.cpp
@@ -1,45 +1,62 @@
 #include <Arduino.h>
-
 const int Full_trame = 11;
 int trame[Full_trame];
 int compteur = 0;
 int tab[5];
-int carte[5] = { 0xC, 0x0, 0x42, 0x16, 0x94 };
-
-void setup() {
+int carte[5] = {0xC, 0x0, 0x42, 0x16, 0x94};
+/////////////////////////////////////////////////////////////////////////////////
+void setup()
+{
   Serial.begin(9600);
   Serial2.begin(9600, SERIAL_8N1, 2, -1); // RX sur port 2, TX désactivé (-1)
+  Serial.println(" ");
   Serial.println("Systeme verouiller, presenter badge");
+  pinMode(5, OUTPUT);
+  digitalWrite(5, LOW);
 }
+/////////////////////////////////////////////////////////////////////////////////
 
+/////////////////////////////////////////////////////////////////////////////////
 void loop() {}
+/////////////////////////////////////////////////////////////////////////////////
 
-void serialEventRun() {
-  while (Serial2.available()) {
+/////////////////////////////////////////////////////////////////////////////////
+void serialEventRun()
+{
+  while (Serial2.available())
+  {
     trame[compteur] = Serial2.read();
     compteur++;
-    if (compteur == Full_trame) {
-      for (int i = 0; i < 5; i++) {
+
+    if (compteur == Full_trame)
+    {
+      for (int i = 0; i < 5; i++)
+      {
         tab[i] = trame[i + 3];
       }
 
       bool whitelist = true;
-      for (int i = 0; i < 5; i++) {
-        if (tab[i] != carte[i]) {
+      for (int i = 0; i < 5; i++)
+      {
+        if (tab[i] != carte[i])
+        {
           whitelist = false;
           break;
         }
       }
 
-      if (whitelist) {
-        for (int i = 0; i < 5; i++) {
-          Serial.println("Carte correcte, Systeme deverouiller ");
-        } 
+      if (whitelist) // Carte correcte
+      {
+        Serial.println("Robot ON");
+        digitalWrite(5, HIGH);
       }
-      else {
-          Serial.println("Carte incorrecte, Systeme verouiller ");
-        }
+      else // Carte incorrecte
+      {
+        Serial.println("Robot OFF");
+        digitalWrite(5, LOW);
+      }
       compteur = 0;
     }
   }
 }
+/////////////////////////////////////////////////////////////////////////////////
