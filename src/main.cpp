df9d300a81c4e7109f99d0b7db44fa946ac6eaec
AKCA Samet <Smt.68.58@gmail.com>
2024-12-10 15:22:14 +0100

    Etat Whitelist

--- src/main.cpp
+++ src/main.cpp
@@ -4,24 +4,41 @@ const int Full_trame = 11;
 int trame[Full_trame];
 int compteur = 0;
 int tab[5];
-int carte[5] = {0xC, 0x0, 0x42, 0x16, 0x94};
+int carte[5] = { 0xC, 0x0, 0x42, 0x16, 0x94 };
 
 void setup() {
   Serial.begin(9600);
+  Serial2.begin(9600, SERIAL_8N1, 2, -1); // RX sur port 2, TX désactivé (-1)
+  Serial.println("Systeme verouiller, presenter badge");
 }
 
 void loop() {}
 
-void serialEvent() {
-  while (Serial.available()) {
-    trame[compteur] = Serial.read();
+void serialEventRun() {
+  while (Serial2.available()) {
+    trame[compteur] = Serial2.read();
     compteur++;
     if (compteur == Full_trame) {
-      for (int i = 0; i < Full_trame; i++) {
-        Serial.print(trame[i], HEX);
-        Serial.print(" ");
+      for (int i = 0; i < 5; i++) {
+        tab[i] = trame[i + 3];
       }
-      Serial.println();
+
+      bool whitelist = true;
+      for (int i = 0; i < 5; i++) {
+        if (tab[i] != carte[i]) {
+          whitelist = false;
+          break;
+        }
+      }
+
+      if (whitelist) {
+        for (int i = 0; i < 5; i++) {
+          Serial.println("Carte correcte, Systeme deverouiller ");
+        } 
+      }
+      else {
+          Serial.println("Carte incorrecte, Systeme verouiller ");
+        }
       compteur = 0;
     }
   }
